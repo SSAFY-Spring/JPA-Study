08abe8cd35e92ef4006dfcc733a29162dddeefba
신영한 <syhan7516@naver.com>
2024-03-06 14:08:55 +0900

    기존 CRUD 테스트를 스프링 데이터 JPA 변환 후 테스트 수행

--- practice/src/test/java/com/jpa/practice/PracticeApplicationTests.java
+++ practice/src/test/java/com/jpa/practice/PracticeApplicationTests.java
@@ -103,35 +103,35 @@ class PracticeApplicationTests {
 		}
 	}
 
-	// 순수 JPA 레포지터리 CRUD 테스트
+	// 순수 JPA 레포지터리에서 스프링 데이터 JPA 변환 후 CRUD 테스트
 	@Test
 	public void testMember4() {
 		Member member1 = new Member("member1");
 		Member member2 = new Member("member2");
-		memberJpaRepository.save(member1);
-		memberJpaRepository.save(member2);
+		memberRepository.save(member1);
+		memberRepository.save(member2);
 
 		// 단건 조회 검증
 		Member findMember1 =
-				memberJpaRepository.findById(member1.getId()).get();
+				memberRepository.findById(member1.getId()).get();
 		Member findMember2 =
-				memberJpaRepository.findById(member2.getId()).get();
+				memberRepository.findById(member2.getId()).get();
 		assertThat(findMember1).isEqualTo(member1);
 		assertThat(findMember2).isEqualTo(member2);
 
 		// 리스트 조회 검증
-		List<Member> all = memberJpaRepository.findAll();
+		List<Member> all = memberRepository.findAll();
 		assertThat(all.size()).isEqualTo(2);
 
 		// 카운트 검증
-		long count = memberJpaRepository.count();
+		long count = memberRepository.count();
 		assertThat(count).isEqualTo(2);
 
 		// 삭제 검증
-		memberJpaRepository.delete(member1);
-		memberJpaRepository.delete(member2);
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
 
-		long deletedCount = memberJpaRepository.count();
+		long deletedCount = memberRepository.count();
 		assertThat(deletedCount).isEqualTo(0);
 	}
 }
