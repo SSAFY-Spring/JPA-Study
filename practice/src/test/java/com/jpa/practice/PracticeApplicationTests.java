d3c56c1af9d4bcc690df930a4036a7c4a8d70900
신영한 <syhan7516@naver.com>
2024-03-06 13:37:45 +0900

    팀 저장, 조회 기능 테스트

--- practice/src/test/java/com/jpa/practice/PracticeApplicationTests.java
+++ practice/src/test/java/com/jpa/practice/PracticeApplicationTests.java
@@ -1,13 +1,18 @@
 package com.jpa.practice;
 
 import com.jpa.practice.entity.Member;
+import com.jpa.practice.entity.Team;
 import com.jpa.practice.repository.MemberJpaRepository;
 import com.jpa.practice.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +22,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 // 테스트에서는 작업 후 기본이 롤백이므로 처리 필요
 @Rollback(value = false)
 class PracticeApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	// 회원 레포지터리 주입
 	@Autowired
@@ -30,8 +38,7 @@ class PracticeApplicationTests {
 	public void testMember1() {
 		
 		// 회원 생성
-		Member member = new Member();
-		member.setUsername("memberA");
+		Member member = new Member("memberA");
 		Member saveMember = memberJpaRepository.save(member);
 		
 		// 회원 조회
@@ -50,8 +57,7 @@ class PracticeApplicationTests {
 	public void testMember2() {
 
 		// 회원 생성
-		Member member = new Member();
-		member.setUsername("memberA");
+		Member member = new Member("memberA");
 		Member saveMember = memberRepository.save(member);
 
 		// 회원 조회
@@ -63,5 +69,36 @@ class PracticeApplicationTests {
 
 		// 동일성 검증
 		assertThat(findMember).isEqualTo(member);
+	}
+
+	@Test
+	public void testMember3() {
+
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+		//초기화
+		em.flush();
+		em.clear();
+
+		//확인
+		List<Member> members = em.createQuery("select m from Member m",
+						Member.class)
+				.getResultList();
+		for (Member member : members) {
+			System.out.println("member=" + member);
+			System.out.println("-> member.team=" + member.getTeam());
+		}
 	}
 }
