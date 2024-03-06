9e41b00b014538ca588bd9d48cf9a3e1205cdbc2
신영한 <syhan7516@naver.com>
2024-03-06 10:53:28 +0900

    회원 레포지터리 생성
    회원 저장, 회원 조회 기능 구현

---
+++ practice/src/main/java/com/jpa/practice/MemberJpaRepository.java
@@ -0,0 +1,24 @@
+package com.jpa.practice;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberJpaRepository {
+
+    // 매니저 가져오기
+    @PersistenceContext
+    private EntityManager em;
+
+    // 회원 저장
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    // 회원 조회
+    private Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
